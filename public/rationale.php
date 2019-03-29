1ce2acc6193868eb4841359fa7fe885fd2882daa
marissagaspari <47733103+marissagaspari@users.noreply.github.com>
2019-03-29 16:50:20 +1100

    Add files via upload

---
+++ public/rationale.php
@@ -0,0 +1,51 @@
+<?php 
+  
+    require "../config.php";
+    require "common.php";
+?>
+<?php include "templates/header.php"; ?>
+<p> 
+Web Application Assignment One
+Back-end Web Development (11058|1)
+Due: 29. March. 2019
+U3160702
+Word count: 662
+<br> 
+Website: http://marissa-assignment.epizy.com/public/
+Github:  https://github.com/marissagaspari/marissa_assignment
+<br> 
+
+Web Application
+The web application that I have chosen to create is based on tracking beauty appointments. I have decided to track beauty appointments as I have not seen many applications on the market and I would find this useful to track appointments and spending. This will allow for all appointments from haircuts to waxing to be uploaded to the website and be easily accessible. 
+<br> 
+Progress through the weeks 
+<br> 
+Week 4
+This week we started to do some coding and build our files for the web application. The server-side language we are using is Php to create our application. This is new to me but is also similar to HTML, which I have used previously so it is slowly coming back to me. I used the classwork that we did to start to put together my assignment. This gave the classwork a purpose and allowed to get a head start on the assignment. All the folders are set up and the basic pages that will be used for the website to allow for the appointments to be loaded onto the website. 
+ <br> 
+Week 5 
+All the folders are set up and it is slowly coming together. The coding is pretty much done. Just have a few things to do. 
+<br> 
+Week 6 
+This week was dedicated to putting the final touches to the back end of the web application. The application seems to be working well and without any errors.  I was able to code the home page title onto each page to allow for the title to take you back to the home page. Issues came up when I deleted too much and had to find the issue, which ended up being something simple like a comma. This taught me to be more careful and how something so simple can change or cause an error on the website.  I finished the back end work this week so next week will be dedicated to the front end and uploading it all. 
+<br> 
+Week 7 
+This week the aim is to create the front end of the application to make it more aesthetically pleasing. The format, colour, and presentation will all be changed to give the website a nicer look and feel.
+<br> 
+I adapted my title from being the home page to adding the home page to the list under the heading. I confused myself and the code by adding a home page as well as a header. So to simplify it I decided to delete the home page coding and just stick with the home page link in the list. 
+<br> 
+I decided to make the prominent colour of the website “palevioletred”. The headings of the page are outlined in this colour and the lists are filled with the colour. 
+<br> 
+The list that I coded is also changed and is now listed horizontally and in a container with the “palevioletred” colour. I created this using the Skeleton code but changed the colour and added the square dotes next to it. I initially found the skeleton coding confusing, as I was not sure how to use it but thankfully had help and was able to understand it in class. 
+<br> 
+After believing that I uploaded it all and all was complete turns out my update page was not working correctly. This made me stumble at the last moment. Still managed to miss out on correcting a simple error but I guess it comes with practice of where you need to look to find the error. All seems to be working now. 
+<br> 
+To complete the upload Infinity Free and File Zilla had to be downloaded to allow the website to go live and work outside of the local server.
+ <br> 
+Overall I am happy with my website and all that I have learned. The application works well and is simple and effective. If I had more time I would add reminders to allow the application to remind you when your next appointment should be and a login page as well.
+    
+ </p>
+
+ 
+<?php include "templates/footer.php"; ?>
+
